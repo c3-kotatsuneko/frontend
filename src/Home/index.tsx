c879217de69e556e711f8c86682c91744669cdf4
Sea10wood <miyusea5@gmail.com>
2024-07-11 22:54:15 +0900

    Add : WelcomePage SetUP

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,10 +1,13 @@
+import { HomeIcon } from "../components/icon/home";
+
 function Home() {
-  return (
-    <div>
-      <h1>hello world</h1>
-      <p>this is a react</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>hello world</h1>
+			<p>this is a react</p>
+			<HomeIcon variant={{ color: "blue" }} />
+		</div>
+	);
 }
 
 export default Home;
