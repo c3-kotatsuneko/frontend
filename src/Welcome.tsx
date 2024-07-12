38671617cb97788b5f348706c796282105baf212
Sea10wood <miyusea5@gmail.com>
2024-07-12 09:01:10 +0900

    Add: WelcomePage  /  welcome_box

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "./welcome.css";
 
 function Welcome() {
   return (
-    <div>
+    <div className="welcome_body">
       <h2>ばーちゃるぼっくす</h2>
+      <div className="welcome_box">
+        <h1>Name</h1>
+        <h1>Password</h1>
+        <button>あそびにいく</button>
+        <a>ゲストとしてあそびにいく</a>
+      </div>
     </div>
   );
 }
