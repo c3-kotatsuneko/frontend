8961d7989d7cbcea013d48068066f77e13fa2f1d
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-07-12 17:20:33 +0900

    ディレクトリ名修正

---
+++ src/components/ui/Input/index.tsx
@@ -0,0 +1,12 @@
+import { ComponentPropsWithRef, ForwardedRef } from "react";
+import styles from "./index.module.css";
+
+type InputProps = {
+    forwardRef?: ForwardedRef<HTMLInputElement>;
+} & ComponentPropsWithRef<"input">;
+
+export const Input = ({forwardRef, ...props}: InputProps) => {
+    return (
+        <input className={styles.inputStyle} ref={forwardRef} {...props} />
+    )
+}
