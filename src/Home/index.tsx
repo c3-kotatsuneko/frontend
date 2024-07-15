1360499f04c0cce47615a37e2285344c0a73766b
Semi-koron <6jimoto6@gmail.com>
2024-07-15 14:51:05 +0900

    fix index.html

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,10 +1,16 @@
+import { lazy, Suspense } from "react";
+const Component = lazy(() => import("../Signup.tsx"));
+
 function Home() {
-	return (
-		<div>
-			<h1>hello world</h1>
-			<p>this is a react</p>
-		</div>
-	);
+  return (
+    <div>
+      <h1>hello world</h1>
+      <p>this is a react</p>
+      <Suspense>
+        <Component onSubmit={() => {}} />
+      </Suspense>
+    </div>
+  );
 }
 
 export default Home;
