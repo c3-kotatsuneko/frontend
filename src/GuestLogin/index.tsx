d303ab112b7185d134f5e422228ebe318bc5d4b8
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-07-15 14:59:52 +0900

    ゲストログインページ

---
+++ src/GuestLogin/index.tsx
@@ -0,0 +1,20 @@
+import { DefaultButton } from "../components/ui/Button";
+import { TextInput } from "../components/ui/TextInput";
+import styles from "./index.module.css";
+
+export const GuestLoginPage = () => {
+  return (
+    <main className={styles.root}>
+      <p className={styles.title}>おなまえをおしえてください</p>
+      <TextInput label="おなまえ" />
+      <DefaultButton color="redorange">あそびにいく</DefaultButton>
+      <img
+        className={styles["cats-image"]}
+        alt="丸ねこタワー"
+        src="catsTower-circle.png"
+        height={216}
+        width={148}
+      />
+    </main>
+  );
+};
