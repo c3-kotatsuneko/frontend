733b59d02f60dbbabc7aa13a901a6ed14b0be65d
Semi-koron <6jimoto6@gmail.com>
2024-07-15 12:40:21 +0900

    fix lint and add AR function

--- src/components/ui/TextInput/index.tsx
+++ src/components/ui/TextInput/index.tsx
@@ -1,13 +1,12 @@
-import { ComponentPropsWithRef } from 'react';
-import styles from './index.module.css';
-import { Input } from '../Input';
-
+import type { ComponentPropsWithRef } from "react";
+import styles from "./index.module.css";
+import { Input } from "../Input";
 
 type TextInputProps = {
   label: string;
-} & ComponentPropsWithRef<'input'>;
+} & ComponentPropsWithRef<"input">;
 
-export const TextInput = ({label, ...props}: TextInputProps) => {
+export const TextInput = ({ label, ...props }: TextInputProps) => {
   return (
     <label className={styles.label}>
       {label}
