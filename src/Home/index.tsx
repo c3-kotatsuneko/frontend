dd8527bcf5e773491f38b4d899950d13a20c32d1
Sea10wood <miyusea5@gmail.com>
2024-07-15 14:05:34 +0900

    Delate: HomeIcon

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,11 +1,8 @@
-import { HomeIcon } from "../components/icon/home";
-
 function Home() {
 	return (
 		<div>
 			<h1>hello world</h1>
 			<p>this is a react</p>
-			<HomeIcon variant={{ color: "blue" }} />
 		</div>
 	);
 }
