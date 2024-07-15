14494303a4ea062db1b5be24396619b2ec276414
Sea10wood <miyusea5@gmail.com>
2024-07-15 16:16:05 +0900

    Fix: ログイン、サインインのコンポーネントに置き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { Route, Routes } from "react-router-dom";
-
 import Home from "./Home";
 import "./App.css";
+import Welcome from "./Welcome/Welcome";
 
-function AppRoutes() {
+const AppRoutes = () => {
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route
+					path="/welcome"
+					element={
+						<Welcome
+							onLoginSubmit={(name: string, password: string) => {
+								console.log(name, password); 
+								throw new Error("Function not implemented.");
+							}}
+							onSignupSubmit={(name: string, password: string) => {
+								console.log(name, password);
+								throw new Error("Function not implemented.");
+							}}
+						/>
+					}
+				/>
 			</Routes>
 		</>
 	);
-}
+};
 
 export default AppRoutes;
