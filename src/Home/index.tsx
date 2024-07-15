16b91d28a81e0eaa0164cd916ce330c16935596b
Semi-koron <6jimoto6@gmail.com>
2024-07-15 16:14:32 +0900

    add !

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -4,7 +4,7 @@ const Component = lazy(() => import("../Signup.tsx"));
 function Home() {
   return (
     <div>
-      <h1>hello world</h1>
+      <h1>hello world!!!!!!!!!!!!!!!!!!!!!!!</h1>
       <p>this is a react</p>
       <Suspense>
         <Component onSubmit={() => {}} />
