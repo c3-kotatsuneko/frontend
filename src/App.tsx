5e4179eecfa612be69939dd9dc007334974b5c88
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-07-15 13:11:33 +0900

    loadingページの背景

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./Home";
 import { LoadingPage } from "./Loading";
-import { CreditFooter } from "./components/ui/CreditFooter";
 import "./App.css";
 
 function AppRoutes() {
   return (
     <>
       <Routes>
-        <Route element={<CreditFooter />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/loading" element={<LoadingPage />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/loading" element={<LoadingPage />} />
       </Routes>
     </>
   );
--- src/components/ui/CreditFooter/index.tsx
+++
@@ -1,7 +0,0 @@
-export const CreditFooter = () => {
-  return (
-    <footer className="credit">
-      <p>Created by こたつねこ</p>
-    </footer>
-  );
-};
