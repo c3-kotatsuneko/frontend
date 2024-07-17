fb92976bad3a32b3f3dfa6718cbb75dc6e51ad95
Sea10wood <miyusea5@gmail.com>
2024-07-17 14:12:07 +0900

    Add: modal＆ゲストanonymous Button

--- src/components/ui/TextButton/index.tsx
+++ src/components/ui/TextButton/index.tsx
@@ -2,26 +2,20 @@ import type { ComponentPropsWithoutRef, ReactNode } from "react";
 import styles from "./index.module.css";
 import clsx from "clsx";
 
-type ButtonProps = {
+type TextButtonProps = {
 	children: ReactNode;
-	variant?: "contained" | "outlined";
-	size?: "sm" | "md";
-	color?: "redorange" | "yellow" | "blue" | "green" | "brown";
 } & ComponentPropsWithoutRef<"button">;
 
-export const DefaultButton = ({
+export const TextButton = ({
 	children,
-	variant = "contained",
-	size = "md",
 	color = "blue",
 	className,
 	...props
-}: ButtonProps) => {
+}: TextButtonProps) => {
 	return (
 		<button
-			className={clsx(styles["button-style"],className)}
-			data-variant={variant}
-			data-size={size}
+			className={clsx(styles["text-button-style"],className)}
+            style={{ fontSize: '12px' }}
 			data-color={color}
 			{...props}
 		>
