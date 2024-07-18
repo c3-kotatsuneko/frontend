9e70727f4692f217807376e1f661a3de7ac0ee0c
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-07-18 14:41:39 +0900

    fix: スマホでみるとテキストが飛び出てる

--- src/ModeSelect/index.tsx
+++ src/ModeSelect/index.tsx
@@ -2,42 +2,42 @@ import { DefaultButton } from "../components/ui/Button";
 import styles from "./index.module.css";
 
 export const ModeSelectPage = () => {
-  return (
-    <main className={styles.root}>
-      <p className={styles["user-name"]}>ユーザー名</p>
+	return (
+		<main className={styles.root}>
+			<p className={styles["user-name"]}>ユーザー名</p>
 
-      <p className={styles.title}>どこであそぶ？</p>
+			<p className={styles.title}>どこであそぶ？</p>
 
-      <div className={styles["button-wrap"]}>
-        <DefaultButton>たいむあたっく</DefaultButton>
+			<div className={styles["button-wrap"]}>
+				<DefaultButton>たいむあたっく</DefaultButton>
 
-        <DefaultButton color="redorange" disabled>
-          いっしょにたいせん
-        </DefaultButton>
+				<DefaultButton color="redorange" disabled>
+					いっしょにたいせん
+				</DefaultButton>
 
-        <DefaultButton color="green" disabled>
-          つみきで脳トレ
-        </DefaultButton>
+				<DefaultButton color="green" disabled>
+					つみきで脳トレ
+				</DefaultButton>
 
-        <DefaultButton variant="outlined" size="sm">
-          <img
-            className={styles["crown-image"]}
-            alt="crown"
-            src="crown.png?url"
-            width={16}
-            height={16}
-          />
-          ランキング
-        </DefaultButton>
-      </div>
+				<DefaultButton variant="outlined" size="sm">
+					<img
+						className={styles["crown-image"]}
+						alt="crown"
+						src="crown.png?url"
+						width={16}
+						height={16}
+					/>
+					ランキング
+				</DefaultButton>
+			</div>
 
-      <img
-        className={styles["cats-image"]}
-        alt="のびねこ"
-        src="/cats/extendedMike.png?url"
-        width={216}
-        height={106}
-      />
-    </main>
-  );
+			<img
+				className={styles["cats-image"]}
+				alt="のびねこ"
+				src="/cats/extendedMike.png?url"
+				width={216}
+				height={106}
+			/>
+		</main>
+	);
 };
--- src/Share/index.tsx
+++ src/Share/index.tsx
@@ -10,59 +10,59 @@ const SAMPLE_RANK = 1;
 const SAMPLE_TIME = "02:10";
 
 export const SharePage = () => {
-  const rank = SAMPLE_RANK;
-  const time = SAMPLE_TIME;
+	const rank = SAMPLE_RANK;
+	const time = SAMPLE_TIME;
 
-  return (
-    <main className={styles.root}>
-      <p className={styles["user-name"]}>ユーザー名</p>
+	return (
+		<main className={styles.root}>
+			<p className={styles["user-name"]}>ユーザー名</p>
 
-      <div className={styles["share-container"]}>
-        <div className={styles["share-text"]}>
-          <img
-            alt="おすわりねこ"
-            src="cats/sitDown.png"
-            width={64}
-            height={65}
-          />
-          <div>
-            <span>おつかれさまにゃ！</span>
-            <span>結果を共有してにゃ〜</span>
-          </div>
-        </div>
+			<div className={styles["share-container"]}>
+				<div className={styles["share-text"]}>
+					<img
+						alt="おすわりねこ"
+						src="cats/sitDown.png"
+						width={64}
+						height={65}
+					/>
+					<div>
+						<span>おつかれさまにゃ！</span>
+						<span>結果を共有してにゃ〜</span>
+					</div>
+				</div>
 
-        <a
-          className={styles["x-share-button"]}
-          href={`http://twitter.com/share?text=ランキング${rank}位!%20タイム${time}!%0a%0a&hashtags=ばーちゃるぼっくす`}
-          target="_blank"
-          rel="nofollow noopener noreferrer"
-        >
-          X で結果をシェアする
-        </a>
-        <a
-          className={styles["line-share-button"]}
-          href={`https://social-plugins.line.me/lineit/share?&text=ランキング${rank}位!%20タイム${time}!%0a%0aばーちゃるぼっくすであそんだよ`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          LINE で結果をシェアする
-        </a>
-      </div>
+				<a
+					className={styles["x-share-button"]}
+					href={`http://twitter.com/share?text=ランキング${rank}位!%20タイム${time}!%0a%0a&hashtags=ばーちゃるぼっくす`}
+					target="_blank"
+					rel="nofollow noopener noreferrer"
+				>
+					X で結果をシェアする
+				</a>
+				<a
+					className={styles["line-share-button"]}
+					href={`https://social-plugins.line.me/lineit/share?&text=ランキング${rank}位!%20タイム${time}!%0a%0aばーちゃるぼっくすであそんだよ`}
+					target="_blank"
+					rel="noreferrer"
+				>
+					LINE で結果をシェアする
+				</a>
+			</div>
 
-      <Link className={styles["return-game"]} to="/mode_select">
-        もういちどあそびにいく
-      </Link>
+			<Link className={styles["return-game"]} to="/mode_select">
+				もういちどあそびにいく
+			</Link>
 
-      <DefaultButton
-        variant="outlined"
-        className={clsx(buttonStyles["button-style"], styles["return-home"])}
-      >
-        <HomeIcon
-          variant={{ color: "blue" }}
-          style={{ width: "24px", height: "24px" }}
-        />
-        おうちへ
-      </DefaultButton>
-    </main>
-  );
+			<DefaultButton
+				variant="outlined"
+				className={clsx(buttonStyles["button-style"], styles["return-home"])}
+			>
+				<HomeIcon
+					variant={{ color: "blue" }}
+					style={{ width: "24px", height: "24px" }}
+				/>
+				おうちへ
+			</DefaultButton>
+		</main>
+	);
 };
