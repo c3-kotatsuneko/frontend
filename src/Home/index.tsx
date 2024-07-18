c5e6cddd4de21dccca4d0384f7004a30c78d8022
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-07-18 16:28:18 +0900

    format

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -3,16 +3,16 @@ import { Timer } from "../components/Timer";
 const Component = lazy(() => import("../SignupForm/Signup"));
 
 function Home() {
-  return (
-    <div>
-      <Timer remainingTime={250} />
-      <h1>hello world!!!!!!!!!!!!!!!!!!!!!!!</h1>
-      <p>this is a react</p>
-      <Suspense>
-        <Component onSubmit={() => {}} />
-      </Suspense>
-    </div>
-  );
+	return (
+		<div>
+			<Timer remainingTime={250} />
+			<h1>hello world!!!!!!!!!!!!!!!!!!!!!!!</h1>
+			<p>this is a react</p>
+			<Suspense>
+				<Component onSubmit={() => {}} />
+			</Suspense>
+		</div>
+	);
 }
 
 export default Home;
