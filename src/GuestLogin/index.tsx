60a5241453d9e7284c19b75c52bef9a5bacb37b7
Sea10wood <miyusea5@gmail.com>
2024-07-18 18:02:16 +0900

    Add: #52_GuestLogin Pageに追加

--- src/GuestLogin/index.tsx
+++ src/GuestLogin/index.tsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import { DefaultButton } from "../components/ui/Button";
 import { TextInput } from "../components/ui/TextInput";
 import styles from "./index.module.css";
 
-export const GuestLoginPage = () => {
+interface GuestLoginProps {
+	onSubmit: (name: string) => void;
+  }
+
+export const GuestLoginPage: React.FC<GuestLoginProps> = ({ onSubmit }) => {
+	const [name, setName] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('hogehoegedayo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name }),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      onSubmit(data.name);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 	return (
 		<main className={styles.root}>
 			<p className={styles.title}>おなまえをおしえてください</p>
-			<TextInput label="おなまえ" />
-			<DefaultButton color="redorange">あそびにいく</DefaultButton>
+			<form onSubmit={handleSubmit}>
+			<TextInput label="おなまえ" value={name} onChange={(e) => setName(e.target.value)} />
+			<DefaultButton color="redorange" type="submit">あそびにいく</DefaultButton>
+			</form>
 			<img
 				className={styles["cats-image"]}
 				alt="丸ねこタワー"
