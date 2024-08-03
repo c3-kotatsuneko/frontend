81e675bd2f9c47f12224aa20af3587ae4b372650
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-08-03 20:49:02 +0900

    fix: imgのpathを修正

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,24 @@ import { PlayTimeAttack } from "./pages/PlayTimeAttack";
 import { PlayStart } from "./pages/PlayStart";
 
 const AppRoutes = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" />
-        <Route index element={<Navigate to="/welcome" replace />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/loading" element={<LoadingPage />} />
-        <Route path="/guest_login" element={<GuestLoginPage />} />
-        <Route path="/mode_select" element={<ModeSelectPage />} />
-        <Route path="/ranking_preview" element={<RankingPreviewPage />} />
-        <Route path="/ranking" element={<RankingPage />} />
-        <Route path="/play_timeAttack" element={<PlayTimeAttack />} />
-        <Route path="/congratulation_share_sns" element={<SharePage />} />
+	return (
+		<>
+			<Routes>
+				<Route path="/" />
+				<Route index element={<Navigate to="/welcome" replace />} />
+				<Route path="/welcome" element={<Welcome />} />
+				<Route path="/loading" element={<LoadingPage />} />
+				<Route path="/guest_login" element={<GuestLoginPage />} />
+				<Route path="/mode_select" element={<ModeSelectPage />} />
+				<Route path="/ranking_preview" element={<RankingPreviewPage />} />
+				<Route path="/ranking" element={<RankingPage />} />
+				<Route path="/play_timeAttack" element={<PlayTimeAttack />} />
+				<Route path="/congratulation_share_sns" element={<SharePage />} />
 
-        <Route path="play_start" element={<PlayStart />} />
-      </Routes>
-    </>
-  );
+				<Route path="play_start" element={<PlayStart />} />
+			</Routes>
+		</>
+	);
 };
 
 export default AppRoutes;
