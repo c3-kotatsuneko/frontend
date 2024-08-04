0a34665ffaa92483dff7922cf8cb24192e7b4465
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-08-04 20:32:33 +0900

    コメントアウトを削除

--- src/pages/Share/index.tsx
+++ src/pages/Share/index.tsx
@@ -1,102 +1,75 @@
 import clsx from "clsx";
 import { Link, useNavigate } from "react-router-dom";
-
 import { HomeIcon } from "../../components/icon/Home";
 import { DefaultButton } from "../../components/ui/Button";
 import buttonStyles from "../../components/ui/Button/index.module.css";
-import styles from "./index.module.css";
-// import { useEffect } from "react";
 import { formatTime } from "../../utils/formatTime";
 import { useTimeAttackStore } from "../../store/useTimeAttackStore";
 import { useUserStore } from "../../store/useUserStore";
+import styles from "./index.module.css";
 
 export const SharePage = () => {
-	const navigate = useNavigate();
-	const { name: userName } = useUserStore();
-	const { clearTime, resultStatus, rank } = useTimeAttackStore();
-	//   const [userRank, setUserRank] = useState(0);
-	// const [clearTime, setClearTime] = useState(0);
-	//   const [resultStatus, setResultStatus] = useState<ResultStatus>({
-	//     isNew: false,
-	//     canRecord: false,
-	//   });
+  const navigate = useNavigate();
+  const { name: userName } = useUserStore();
+  const { clearTime, resultStatus, rank } = useTimeAttackStore();
 
-	// useEffect(() => {
-	// const getUserRank = Number(localStorage.getItem("userRank"));
-	// const getClearTime = Number(localStorage.getItem("clearTime"));
-	// const getResultStatus = localStorage.getItem("resultStatus");
-	// if (getUserRank) {
-	//   setUserRank(getUserRank);
-	// }
-	// if (getClearTime) {
-	// 	setClearTime(getClearTime);
-	// }
-	// if (getResultStatus) {
-	//   setResultStatus(JSON.parse(getResultStatus));
-	// }
-	// userRankが-1の場合はランキングに登録されていない
-	// if (getUserRank === -1) {
-	//   setUserRank(0);
-	// }
-	// }, []);
+  const shareMessage = resultStatus?.isNew
+    ? `自己ベスト更新！タイム${formatTime(clearTime)}!`
+    : rank
+      ? `ランキング${rank}位！ タイム${formatTime(clearTime)}！`
+      : `タイムは${formatTime(clearTime)}！`;
 
-	const shareMessage = resultStatus?.isNew
-		? `自己ベスト更新！タイム${formatTime(clearTime)}!`
-		: rank
-			? `ランキング${rank}位！ タイム${formatTime(clearTime)}！`
-			: `タイムは${formatTime(clearTime)}！`;
+  const encodedLineShareMessage = encodeURIComponent(
+    `${shareMessage}\n\nばーちゃるぼっくすであそんだよ`
+  );
 
-	const encodedLineShareMessage = encodeURIComponent(
-		`${shareMessage}\n\nばーちゃるぼっくすであそんだよ`,
-	);
+  return (
+    <main className={styles.root}>
+      <p className={styles["user-name"]}>{userName}</p>
+      <div className={styles["share-container"]}>
+        <div className={styles["share-text"]}>
+          <img
+            alt="おすわりねこ"
+            src="cats/sitDown.png"
+            width={64}
+            height={65}
+          />
+          <div>おつかれさまにゃ！ 結果を共有してにゃ〜</div>
+        </div>
 
-	return (
-		<main className={styles.root}>
-			<p className={styles["user-name"]}>{userName}</p>
-			<div className={styles["share-container"]}>
-				<div className={styles["share-text"]}>
-					<img
-						alt="おすわりねこ"
-						src="cats/sitDown.png"
-						width={64}
-						height={65}
-					/>
-					<div>おつかれさまにゃ！ 結果を共有してにゃ〜</div>
-				</div>
+        <a
+          className={styles["x-share-button"]}
+          href={`http://twitter.com/share?text=${shareMessage}%0a%0a&hashtags=ばーちゃるぼっくす`}
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+        >
+          X で結果をシェアする
+        </a>
+        <a
+          className={styles["line-share-button"]}
+          href={`https://line.me/R/share?text=${encodedLineShareMessage}`}
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+        >
+          LINE で結果をシェアする
+        </a>
+      </div>
 
-				<a
-					className={styles["x-share-button"]}
-					href={`http://twitter.com/share?text=${shareMessage}%0a%0a&hashtags=ばーちゃるぼっくす`}
-					target="_blank"
-					rel="nofollow noopener noreferrer"
-				>
-					X で結果をシェアする
-				</a>
-				<a
-					className={styles["line-share-button"]}
-					href={`https://line.me/R/share?text=${encodedLineShareMessage}`}
-					target="_blank"
-					rel="nofollow noopener noreferrer"
-				>
-					LINE で結果をシェアする
-				</a>
-			</div>
-
-			<Link className={styles["return-game"]} to="/guest_login">
-				もういちどあそびにいく
-			</Link>
-			<DefaultButton
-				variant="outlined"
-				size="sm"
-				className={clsx(buttonStyles["button-style"], styles["return-home"])}
-				onClick={() => navigate("/mode_select")}
-			>
-				<HomeIcon
-					variant={{ color: "blue" }}
-					style={{ width: "24px", height: "24px" }}
-				/>
-				おうちへ
-			</DefaultButton>
-		</main>
-	);
+      <Link className={styles["return-game"]} to="/guest_login">
+        もういちどあそびにいく
+      </Link>
+      <DefaultButton
+        variant="outlined"
+        size="sm"
+        className={clsx(buttonStyles["button-style"], styles["return-home"])}
+        onClick={() => navigate("/mode_select")}
+      >
+        <HomeIcon
+          variant={{ color: "blue" }}
+          style={{ width: "24px", height: "24px" }}
+        />
+        おうちへ
+      </DefaultButton>
+    </main>
+  );
 };
