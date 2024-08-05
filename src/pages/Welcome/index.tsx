490cdd95cfde71faa424c5b628934c62ae777653
Sea10wood <miyusea5@gmail.com>
2024-08-05 22:33:27 +0900

    add: #21pdf-download function

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -2,10 +2,15 @@ import Tab from "../../components/features/welcome/tab";
 import Signup from "./items/SignupForm";
 import Login from "./items/LoginForm";
 import styles from "./index.module.css";
+import { DownloadButton } from "../../components/features/welcome/DownloadButton";
 
 const Welcome = () => {
 	return (
 		<main className={styles["welcome-body"]}>
+			<div className={styles['download-button']}>
+
+			<DownloadButton />
+			</div>
 			<h2>ばーちゃるぼっくす</h2>
 			<Tab
 				labels={["ろぐいん", "とうろく"]}
---
+++ src/utils/downloads.ts
@@ -0,0 +1,25 @@
+const filesToDownload = [
+    { blob: new URL('../../public/marker/marker_file.pdf', import.meta.url).href, filename: 'marker_file.pdf' },
+  ];
+  
+  const downloadBlob = (blob: string, filename: string) => {
+    const link = document.createElement('a');
+    link.href = blob;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  
+  export const handleDownload = () => {
+    for (const file of filesToDownload) {
+      fetch(file.blob)
+        .then(res => res.blob())
+        .then(blob => {
+          const url = URL.createObjectURL(blob);
+          downloadBlob(url, file.filename);
+        })
+        .catch(err => console.error("Failed to download pdf", err));
+    }
+  };
+  
