f8dd5b5daa873835aedf38ffd03a072f55ed14ad
Sea10wood <miyusea5@gmail.com>
2024-08-06 22:17:24 +0900

    add: go_home_Pageの作成

--- src/pages/GoHome/index.tsx
+++ src/pages/GoHome/index.tsx
@@ -1,17 +1,16 @@
-import styles from "./index.module.css"
+import styles from "./index.module.css";
 
 export const GoHome = () => {
-    return (
-        <div>
-
-            <h4>きをつけておうちに帰ってね</h4>
-            <img
-                className={styles["cats-image"]}
-                alt="丸ねこタワー"
-                src="/cats/catsTower-circle.webp"
-                width={120}
-                height={180}
-            />
-        </div>
-    )
-}
+	return (
+		<div>
+			<p className={styles["home-message"]}>きをつけておうちに帰ってね</p>
+			<img
+				className={styles["cats-image"]}
+				alt="丸ねこタワー"
+				src="/cats/catsTower-circle.webp"
+				width={120}
+				height={180}
+			/>
+		</div>
+	);
+};
