a4f4af450c1e4ff955ee4d7756579a281d879177
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-08-25 16:45:29 +0900

    無限レンダリングで生成される要素を消す処理を削除

--- src/pages/PlayTimeAttack/index.tsx
+++ src/pages/PlayTimeAttack/index.tsx
@@ -9,48 +9,27 @@ import { useCountTimer } from "./hooks/useCountTimer";
 import styles from "./index.module.css";
 
 export const PlayTimeAttack = () => {
-	const navigate = useNavigate();
-	const setClearTime = useTimeAttackStore((state) => state.setClearTime);
-	const { name: userName } = useUserStore();
-	const { time } = useCountTimer(10);
+  const navigate = useNavigate();
+  const setClearTime = useTimeAttackStore((state) => state.setClearTime);
+  const { name: userName } = useUserStore();
+  const { time } = useCountTimer(10);
 
-	//TODO: 無限レンダリングを直す。これは無限レンダリングにより生成される要素を一時的に削除している
-	useEffect(() => {
-		const cleanUp = () => {
-			const videoElements = document.querySelectorAll("video");
-			for (const element of videoElements) {
-				element.remove();
-			}
-			const canvasElements = document.querySelectorAll("canvas");
-			for (const element of canvasElements) {
-				element.remove();
-			}
-			const scriptElements = document.querySelectorAll("script");
-			for (const element of scriptElements) {
-				element.remove();
-			}
-		};
-		return () => {
-			cleanUp();
-		};
-	}, []);
+  // timeが0になったらrankingページへ遷移する
+  useEffect(() => {
+    // TODO: クリアタイムを取得する
+    setClearTime(18);
 
-	// timeが0になったらrankingページへ遷移する
-	useEffect(() => {
-		// TODO: クリアタイムを取得する
-		setClearTime(18);
+    if (time === 0) {
+      navigate("/ranking");
+    }
+  }, [navigate, time, setClearTime]);
 
-		if (time === 0) {
-			navigate("/ranking");
-		}
-	}, [navigate, time, setClearTime]);
-
-	return (
-		<main>
-			<Timer remainingTime={time} />
-			<p className={styles["user-name"]}>{userName}</p>
-			<ARApp />
-			<ExitButton />
-		</main>
-	);
+  return (
+    <main>
+      <Timer remainingTime={time} />
+      <p className={styles["user-name"]}>{userName}</p>
+      <ARApp />
+      <ExitButton />
+    </main>
+  );
 };
