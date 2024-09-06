47905a7b27b2a34a9d9fdf2fd70fce7f13219caa
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-09-06 18:33:23 +0900

    リザルトタイムの表示を修正

--- src/pages/MultiWaiting/index.tsx
+++ src/pages/MultiWaiting/index.tsx
@@ -1,11 +1,13 @@
 import { useTimeAttackStore } from "../../store/useTimeAttackStore";
 import { useUserStore } from "../../store/useUserStore";
+import { formatTime } from "../../utils/formatTime";
 import styles from "./index.module.css";
 
 export const MultiWaitingPage = () => {
 	const userName = useUserStore((state) => state.name);
 	const clearTime = useTimeAttackStore((state) => state.clearTime);
 	const rank = useTimeAttackStore((state) => state.rank);
+	const formattedTime = formatTime(clearTime);
 
 	return (
 		<main className={styles.root}>
@@ -13,7 +15,7 @@ export const MultiWaitingPage = () => {
 			<p className={styles.result}>
 				{rank}位 {userName}
 			</p>
-			<p className={styles["result-time"]}>{clearTime}</p>
+			<p className={styles["result-time"]}>{formattedTime}</p>
 			<p className={styles.message}>
 				他のおともだちが終わるまでちょっとまってね
 			</p>
