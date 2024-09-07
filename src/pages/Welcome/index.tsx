af77efff475d9c83ca66e99cf349905262c59d39
Tanakaryuki <tnk3257.rt@gmail.com>
2024-09-07 13:19:15 +0900

    fix: デバッグするよ

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -8,37 +8,37 @@ import { DownloadButton } from "../../components/features/welcome/DownloadButton
 import { useSocketRefStore } from "../../store/useSocketRefStore";
 
 const Welcome = () => {
-	const [searchParams] = useSearchParams();
-	const setRoomId = useSocketRefStore((state) => state.setRoomId);
+  const [searchParams] = useSearchParams();
+  const setRoomId = useSocketRefStore((state) => state.setRoomId);
 
-	useEffect(() => {
-		const roomIdParam = searchParams.get("roomId");
-		if (roomIdParam) {
-			setRoomId(roomIdParam);
-			console.log("Room ID 取得", roomIdParam);
-		}
-	}, [searchParams, setRoomId]);
+  useEffect(() => {
+    const roomIdParam = searchParams.get("roomId");
+    if (roomIdParam) {
+      setRoomId(roomIdParam);
+      console.log("Room ID 取得", roomIdParam);
+    }
+  }, [searchParams, setRoomId]);
 
-	return (
-		<main className={styles["welcome-body"]}>
-			<div className={styles["download-button"]}>
-				<DownloadButton />
-			</div>
-			<h2>ばーちゃるぼっくす</h2>
-			<Tab
-				labels={["ろぐいん", "とうろく"]}
-				contents={[<Login key="login" />, <Signup key="signup" />]}
-				id={""}
-			/>
-			<img
-				src="/cats/catsTower-circle.webp"
-				className={styles["main-logo"]}
-				alt="logo"
-				width="84"
-				height="132"
-			/>
-		</main>
-	);
+  return (
+    <main className={styles["welcome-body"]}>
+      <div className={styles["download-button"]}>
+        <DownloadButton />
+      </div>
+      <h2>デバッグするよ~</h2>
+      <Tab
+        labels={["ろぐいん", "とうろく"]}
+        contents={[<Login key="login" />, <Signup key="signup" />]}
+        id={""}
+      />
+      <img
+        src="/cats/catsTower-circle.webp"
+        className={styles["main-logo"]}
+        alt="logo"
+        width="84"
+        height="132"
+      />
+    </main>
+  );
 };
 
 export default Welcome;
