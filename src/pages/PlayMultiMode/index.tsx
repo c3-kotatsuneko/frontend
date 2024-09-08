673ba3ecc17561e6646c154867c5e77654cad357
Semi-koron <6jimoto6@gmail.com>
2024-09-08 13:56:16 +0900

    multi entranceにplay_multiと同じシステム導入

--- src/pages/PlayMultiMode/index.tsx
+++ src/pages/PlayMultiMode/index.tsx
@@ -14,6 +14,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { HandPosToDataConverter } from "../../components/features/AR/Converter";
 import { useARToolkit } from "../AR/hooks/useARTools";
 import scanStyles from "../../components/features/AR/scan/index.module.css";
+import { ObjectSetting } from "../AR/DataToPosConverter";
 
 const ARScanner = () => {
   const navigate = useNavigate();
@@ -74,38 +75,30 @@ export const PlayMultiMode = () => {
   const handLandMarkerRef = useRef<HandLandmarker | null>(null);
   const handResultRef = useRef<HandLandmarkerResult | null>(null);
   const param = new URLSearchParams(useLocation().search);
-  const position = param.get("position") as "front" | "left" | "right" | "back";
+  const position = (param.get("position") ?? "front") as Position;
   console.log(position);
-  let marker = "../../../public/pattern-frontMarker.patt";
-  switch (position) {
-    case "front":
-      marker = "../../../public/pattern-frontMarker.patt";
-      break;
-    case "left":
-      marker = "../../../public/pattern-leftMarker.patt";
-      break;
-    case "right":
-      marker = "../../../public/pattern-rightMarker.patt";
-      break;
-    case "back":
-      marker = "../../../public/pattern-backMarker.patt";
-      break;
-  }
+  const marker = `/pattern-${position}Marker.patt`;
+
+  const { rendererRef, sceneRef, cameraRef, handBlock, allBlockSet } =
+    ThreeInit(); //lightRef, allBlockSet,をlint回避のため一度削除
+  const tt = document.createElement("canvas");
+  tt.width = window.innerWidth;
+  tt.height = window.innerHeight;
+  const { arToolkitSource, arToolkitContext } = useARToolkit({
+    camera: cameraRef.current ?? new THREE.Camera(),
+    cameraParaDatURL: cameraPara,
+    domElement: rendererRef.current?.domElement ?? tt,
+    markerPatternURL: marker,
+    scene: sceneRef.current ?? new THREE.Scene(),
+  });
+
   useEffect(() => {
     handInit(handLandMarkerRef);
   }, []);
   useEffect(() => {
+    handCameraRef.current = arToolkitSource.domElement;
     predictWebcam(handLandMarkerRef, handCameraRef, handResultRef);
-  }, []);
-  const { rendererRef, sceneRef, cameraRef, handBlock } = ThreeInit(); //lightRef, allBlockSet,をlint回避のため一度削除
-  const { arToolkitSource, arToolkitContext } = useARToolkit({
-    camera: cameraRef.current ?? new THREE.Camera(),
-    cameraParaDatURL: cameraPara,
-    domElement:
-      rendererRef.current?.domElement ?? document.createElement("canvas"),
-    markerPatternURL: marker,
-    scene: sceneRef.current ?? new THREE.Scene(),
-  });
+  }, [arToolkitSource]);
 
   const animate = useCallback(() => {
     if (position !== null) {
@@ -123,7 +116,6 @@ export const PlayMultiMode = () => {
         }
       }
     }
-
     if (rendererRef.current && sceneRef.current && cameraRef.current) {
       rendererRef.current.render(sceneRef.current, cameraRef.current);
       if (arToolkitSource.ready) {
@@ -131,6 +123,7 @@ export const PlayMultiMode = () => {
         sceneRef.current.visible = cameraRef.current.visible;
       }
     }
+    console.log(handResultRef.current);
     requestAnimationFrame(animate);
   }, [
     arToolkitContext,
@@ -141,6 +134,7 @@ export const PlayMultiMode = () => {
     position,
     handBlock,
   ]);
+
   useEffect(() => {
     animate();
   }, [animate]);
@@ -148,9 +142,8 @@ export const PlayMultiMode = () => {
   return (
     <>
       <ARScanner />
-      <video ref={handCameraRef} id="video" autoPlay muted>
-        <track kind="captions" src="" label="No captions available" default />
-      </video>
+      <div id="wrapper" />
+      <ObjectSetting position={position} allBlockSet={allBlockSet} />
     </>
   );
 };
