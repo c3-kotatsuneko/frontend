6b78e3427b7f4da7c9414502b6182bc09833f67d
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-11-09 03:06:34 +0900

    welcomeとloadingのイラストを差し替え

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -1,16 +1,10 @@
 import styles from "./index.module.css";
 
 export const LoadingPage = () => {
-	return (
-		<main className={styles.root}>
-			<p className={styles["loading-message"]}>ちょっと待ってね</p>
-			<img
-				className={styles["cats-image"]}
-				alt="丸ねこタワー"
-				src="/cats/catsTower-circle.webp"
-				width={148}
-				height={242}
-			/>
-		</main>
-	);
+  return (
+    <main className={styles.root}>
+      <p className={styles["loading-message"]}>ちょっと待ってね</p>
+      <img alt="丸ねこタワー" src="/cats/boss.webp" width={182} height={242} />
+    </main>
+  );
 };
--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -8,37 +8,51 @@ import { DownloadButton } from "../../components/features/welcome/DownloadButton
 import { useSocketRefStore } from "../../store/useSocketRefStore";
 
 const Welcome = () => {
-	const [searchParams] = useSearchParams();
-	const setRoomId = useSocketRefStore((state) => state.setRoomId);
+  const [searchParams] = useSearchParams();
+  const setRoomId = useSocketRefStore((state) => state.setRoomId);
 
-	useEffect(() => {
-		const roomIdParam = searchParams.get("roomId");
-		if (roomIdParam) {
-			setRoomId(roomIdParam);
-			console.log("Room ID 取得", roomIdParam);
-		}
-	}, [searchParams, setRoomId]);
+  useEffect(() => {
+    const roomIdParam = searchParams.get("roomId");
+    if (roomIdParam) {
+      setRoomId(roomIdParam);
+      console.log("Room ID 取得", roomIdParam);
+    }
+  }, [searchParams, setRoomId]);
 
-	return (
-		<main className={styles["welcome-body"]}>
-			<div className={styles["download-button"]}>
-				<DownloadButton />
-			</div>
-			<h2>ばーちゃるぼっくす</h2>
-			<Tab
-				labels={["ろぐいん", "とうろく"]}
-				contents={[<Login key="login" />, <Signup key="signup" />]}
-				id={""}
-			/>
-			<img
-				src="/cats/catsTower-circle.webp"
-				className={styles["main-logo"]}
-				alt="logo"
-				width="84"
-				height="132"
-			/>
-		</main>
-	);
+  return (
+    <main className={styles["welcome-body"]}>
+      <div className={styles["download-button"]}>
+        <DownloadButton />
+      </div>
+      <h2>ばーちゃるぼっくす</h2>
+      <Tab
+        labels={["ろぐいん", "とうろく"]}
+        contents={[<Login key="login" />, <Signup key="signup" />]}
+        id={""}
+      />
+      <img
+        src="/cats/boss.webp"
+        className={styles["boss-cat"]}
+        alt="logo"
+        width="98"
+        height="130"
+      />
+      <img
+        src="/cats/maru.webp"
+        className={styles["maru-cat"]}
+        alt="logo"
+        width="110"
+        height="50"
+      />
+      <img
+        src="/cats/sikaku.webp"
+        className={styles["sikaku-cat"]}
+        alt="logo"
+        width="95"
+        height="50"
+      />
+    </main>
+  );
 };
 
 export default Welcome;
