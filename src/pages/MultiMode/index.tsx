d429e019afc17a61c752b547deae0c7b2a8ba325
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-11-09 13:12:24 +0900

    multi modeのpositionクエリで場所指定できるように変更した

--- src/pages/MultiMode/index.tsx
+++ src/pages/MultiMode/index.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { DefaultButton } from "../../components/ui/Button";
 import { useUserStore } from "../../store/useUserStore";
 import styles from "./index.module.css";
@@ -7,8 +7,26 @@ import { useSocketRefStore } from "../../store/useSocketRefStore";
 export const MultiModePage = () => {
 	const navigate = useNavigate();
 	const { name: userName } = useUserStore();
+	const { setDirection } = useSocketRefStore();
 	// TODO: useSocketRefStoreを更新し、positionを取得する
-	const position = useSocketRefStore((state) => state.eventState.direction);
+	//   const position = useSocketRefStore((state) => state.eventState.direction);
+	const param = new URLSearchParams(useLocation().search);
+	const paramPosition = param.get("position") ?? "front";
+
+	const getPositionValue = (position: string) => {
+		switch (position) {
+			case "front":
+				return 1;
+			case "back":
+				return 2;
+			case "left":
+				return 3;
+			case "right":
+				return 4;
+			default:
+				return 0;
+		}
+	};
 
 	return (
 		<main className={styles.root}>
@@ -16,14 +34,17 @@ export const MultiModePage = () => {
 			<p className={styles.message}>ここに移動してね</p>
 			<img
 				className={styles.marker}
-				src={`/marker/${position}.png`}
-				alt={position}
+				src={`/marker/${paramPosition}.png`}
+				alt={paramPosition}
 				width={300}
 				height={300}
 			/>
 			<DefaultButton
 				color="redorange"
-				onClick={() => navigate(`/multi_Entrance?position=${position}`)}
+				onClick={() => {
+					setDirection(getPositionValue(paramPosition));
+					navigate(`/multi_Entrance?position=${paramPosition}`);
+				}}
 			>
 				いどう、したよ！
 			</DefaultButton>
